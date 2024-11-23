e6b8488f9f1c489ccfd43d06ae04e5bf2ac0b41b
raju kumar <rajusharmadata123@gamil.com>
2024-11-23 11:09:05 +0530

    add

--- pairsum.java
+++ pairsum.java
@@ -1,6 +1,7 @@
 public class pairsum {
      public static int [] Parisum(int arr[] ,int target){
-        // brute force 
+        /*
+         * //  1. brute force 
         for(int i = 0;i<arr.length;i++){
             for(int j = i+1;j<arr.length;j++){
                 if(arr[i] + arr[j]  == target){
@@ -9,10 +10,28 @@ public class pairsum {
             }
         }
         return new int[] {-1,-1};
+         */
+
+         // Two pointer aproach 
+        int i = 0;
+        int j = arr.length-1;
+        int sum ;
+        while(i<j){
+            sum = arr[i] + arr[j];
+            if(sum>target){
+                j--;
+            }else if(sum<target){
+                i++;
+            }else{
+                return new int[]{i,j};
+            }
+        }
+        return new int[]{-1,-1};
+
      }
     public static void main(String[] args) {
         int arr[] = {2,7,11,15};
-        int target = 20;
+        int target = 26;
         int index [] = Parisum(arr, target);
         for(int i = 0; i<index.length;i++){
             System.out.print(index[i]+" ");
