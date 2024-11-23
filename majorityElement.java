6e874b1b5ef87d4a4507b309be7f0c75886f018b
raju kumar <rajusharmadata123@gamil.com>
2024-11-23 12:24:14 +0530

    add

--- majorityElement.java
+++ majorityElement.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class majorityElement {
     public static int Majorityelement(int arr[]) {
         /*
@@ -19,11 +20,30 @@ public class majorityElement {
         }
          */
       
+
+         // optimal Aproach 
+         Arrays.sort(arr);
+        //  for(int i = 0;i<arr.length;i++){
+        //     System.out.print(arr[i] + " ");
+        //  }
+         int frequence =1;
+
+         for(int i = 0;i<arr.length;i++){
+            if(arr[i] == arr[i+1]){
+                frequence++;
+            }else{
+                frequence =1;
+            }
+            if(frequence>(arr.length/2)){
+                return arr[i];
+            }
+         }
+         
         return -1;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, 2,3,3,3,3,3};
+        int arr[] = { 1,2};
         int element = Majorityelement(arr);
         System.out.println(element);
     }
